7825618569120de9de77012eb3faab7175d188db
bedirhanakinn <bedirhanakinn@gmail.com>
2026-06-23 13:28:02 +0200

    Perfect parallax + frog sprite changed

--- WitchEscapeGame/Assets/Scripts/ParallaxLooper.cs
+++ WitchEscapeGame/Assets/Scripts/ParallaxLooper.cs
@@ -5,6 +5,7 @@ public class ParallaxLooper : MonoBehaviour
     [Header("Background Pieces")]
     public Transform bg1;
     public Transform bg2;
+    public Transform bg3;
 
     [Header("Movement")]
     public float baseSpeed = 2f;
@@ -25,11 +26,8 @@ public class ParallaxLooper : MonoBehaviour
         currentSpeed = baseSpeed;
 
         SpriteRenderer sr = bg1.GetComponent<SpriteRenderer>();
-
-        // Automatically get sprite width in world units
         bgWidth = sr.bounds.size.x;
 
-        // Initial positions
         bg1.position = new Vector3(startX, bg1.position.y, bg1.position.z);
 
         bg2.position = new Vector3(
@@ -37,37 +35,43 @@ public class ParallaxLooper : MonoBehaviour
             bg2.position.y,
             bg2.position.z
         );
+
+        bg3.position = new Vector3(
+            startX + (bgWidth * 2f),
+            bg3.position.y,
+            bg3.position.z
+        );
     }
 
     void Update()
     {
-        // Increase speed over time
         currentSpeed += acceleration * Time.deltaTime;
         currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
 
         float move = currentSpeed * speedMultiplier * Time.deltaTime;
 
-        // Move backgrounds left
         bg1.position += Vector3.left * move;
         bg2.position += Vector3.left * move;
+        bg3.position += Vector3.left * move;
 
-        // Loop bg1
-        if (bg1.position.x <= startX - bgWidth)
+        LoopBackground(bg1);
+        LoopBackground(bg2);
+        LoopBackground(bg3);
+    }
+
+    void LoopBackground(Transform bg)
+    {
+        if (bg.position.x <= startX - bgWidth)
         {
-            bg1.position = new Vector3(
-                bg2.position.x + bgWidth,
-                bg1.position.y,
-                bg1.position.z
+            float furthestX = Mathf.Max(
+                bg1.position.x,
+                Mathf.Max(bg2.position.x, bg3.position.x)
             );
-        }
 
-        // Loop bg2
-        if (bg2.position.x <= startX - bgWidth)
-        {
-            bg2.position = new Vector3(
-                bg1.position.x + bgWidth,
-                bg2.position.y,
-                bg2.position.z
+            bg.position = new Vector3(
+                furthestX + bgWidth,
+                bg.position.y,
+                bg.position.z
             );
         }
     }
